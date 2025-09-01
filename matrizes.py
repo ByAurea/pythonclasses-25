86fac13f9630532b188354a38f47cda53850dac1
tigrelau <lauratigreamaral@gmail.com>
2025-09-01 19:57:20 -0300

    add: matrizes file

---
+++ matrizes.py
@@ -0,0 +1,9 @@
+def criar_matriz(linhas, colunas):
+    matriz = []
+    for i in range(linhas):
+        linha = []
+        for j in range(colunas):
+            linha.append('.')
+        matriz.append(linha)
+    return matriz
+
