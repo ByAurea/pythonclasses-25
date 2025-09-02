d0576b20aa11719ce7efd4038cfe8b3d054f21b9
tigrelau <lauratigreamaral@gmail.com>
2025-09-01 21:40:15 -0300

    add: content in matrizes

--- matrizes.py
+++ matrizes.py
@@ -1,3 +1,20 @@
+# def criar_matriz(linhas, colunas):
+#     matriz = []
+#     for i in range(linhas):
+#         linha = []
+#         for j in range(colunas):
+#             linha.append('.')
+#         matriz.append(linha)
+#     return matriz
+
+# def diagonal_inversa(n):
+#     m = criar_matriz(n,n)
+#     coluna = n-1
+#     for linha in range(0,n): #0,1,2,3...n-1
+#         m[linha][coluna] = 'x'
+#         coluna = coluna -1
+#     return m 
+
 def criar_matriz(linhas, colunas):
     matriz = []
     for i in range(linhas):
@@ -7,3 +24,15 @@ def criar_matriz(linhas, colunas):
         matriz.append(linha)
     return matriz
 
+def m_borda(linhas, colunas):
+    matriz = criar_matriz(linhas, colunas)
+    # preencher a primeira linha 
+    for i in range(colunas):
+        matriz[0][i] = 'x'
+        matriz[linhas-1][i] = 'x'
+        for i in range(linhas):
+            matriz[i][0] = 'x'
+            matriz[i][colunas-1] = 'x'
+    return matriz
+
+print (m_borda(4,3))
